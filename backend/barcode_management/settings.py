4b9d44beba115f2528d4539b036c6bde3b489858
AmothissacrajT <amothissacrajt@gmail.com>
2026-07-19 13:02:08 +0530

    fix: Production settings backend

--- backend/barcode_management/settings.py
+++ backend/barcode_management/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["textile-barcode-app.onrender.com",]
 
 
 # Application definition
@@ -100,11 +100,12 @@ tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+        'OPTION': dict(parse_qsl(tmpPostgres.query)),
     }
 }
 
@@ -155,7 +156,6 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://textile-barcode-app.vercel.app",
-    "http://localhost:5173",
 ]
 
 #Cache
